d489d017c87c3b468a69634270f4fcadfb72908c
RetrOO1 <yuui.meikoto@mail.ru>
2023-03-29 17:08:01 +0300

    cpu content

---
+++ front/web/src/components/CpuContent.vue
@@ -0,0 +1,18 @@
+!<template>
+    <div>
+        <cpu-item/>
+    </div>
+</template>
+
+<script>
+import CpuItem from './CpuItem.vue';
+    export default {
+        components: {
+            CpuItem
+        }
+    }
+</script>
+
+<style>
+
+</style>
