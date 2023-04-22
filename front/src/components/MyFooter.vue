4b93bdb46e660ca5df5e86e0530a5719fa34a77c
Aresse <vlados01channel@gmail.com>
2023-04-22 20:30:30 +0300

    up

--- front/src/components/MyFooter.vue
+++ front/src/components/MyFooter.vue
@@ -1,40 +1,7 @@
 <template>
     <footer>
 		<div class="footer">
-			<div class="footer_contacts">
-				<h2>Контакты</h2>
-				<a class="footter_sup" href="">Адреса магазинов</a><br>
-				<a class="footter_sup" href="">+7(981)459-09-38</a>
-				<select class="city_change" name="" id="">Выбор города
-					<option value="">Петрозаводск</option>
-					<option value="">Хабаровск</option>
-					<option value="">Саратов</option>
-					<option value="">Калуга</option>
-				</select><br>
-				<button>
-					<a class="footter_sup" href="">Задать вопрос</a>
-				</button>
-				<div class="add_cpu"><a href="/add_cpu">admin add cpu</a></div>
-			</div>
-			<div class="footer_info">
-				<ul class="footer_ul_info">
-					<a class="footter_sup" href=""><li>Журнал</li></a>
-					<a class="footter_sup" href=""><li>Акции</li></a>
-                    <a class="footter_sup" href=""><li>Информация</li></a>
-                    <a class="footter_sup" href=""><li>Доставка</li></a>
-                    <a class="footter_sup" href=""><li>Гарантия</li></a>
-                    <a class="footter_sup" href=""><li>Кредит и рассрочка</li></a>
-				</ul>
-				<ul class="footer_support">
-					<a class="footter_sup" href=""><li>Сервисные центры</li></a>
-					<a class="footter_sup" href=""><li>Услуги</li></a>
-					<a class="footter_sup" href=""><li>Корпоративным клиентам</li></a>
-					<a class="footter_sup" href=""><li>Аренда помещений</li></a>
-					<a class="footter_sup" href=""><li>Партнёрская программа</li></a>
-					<a class="footter_sup" href=""><li>Обзоры</li></a>
-					<a class="footter_sup" href=""><li>Форум</li></a>
-				</ul>
-			</div>
+			
 		</div>
 	</footer>
 </template>
@@ -46,46 +13,9 @@
 </script>
 
 <style>
-.footer_info {
-    display: flex;
-    margin-left: 40px;
-}
-.footer_support {
-    margin-left:120px;
-    list-style: none
-}
 .footer {
     display: flex;
 	color: rgb(0, 0, 0);
 	justify-content: space-between;
-}
-.footer_ul_info{
-  list-style: none;
-  color: rgb(0, 0, 0);
-}
-.footter_sup {
-    text-decoration: none;
-    color: rgb(0, 0, 0);
-    font-weight: 440;
-    cursor: pointer;
-}
- 
-.footer_ul_docs {
-    list-style: none;
-    color: rgb(0, 0, 0);
-    
-}
-.footter_docs {
-    text-decoration: none;
-    color: rgb(0, 0, 0);
-    cursor: pointer;
-}
-
-.add_cpu {
-	padding-top: 10px;
-}
-
-.add_cpu a {
-	color: rgba(128, 0, 128, 0.5);
 }
 </style>
